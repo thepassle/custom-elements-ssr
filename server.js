6958cac9ec62f429ad3d9001b3beb5529ac392d9
thepassle <pascalschilp@gmail.com>
2022-04-21 20:22:11 +0200

    chore: formatting

--- server.js
+++ server.js
@@ -3,44 +3,44 @@ import { DOMParser } from 'linkedom';
 import { CustomElementRender } from './CustomElementRenderer.js';
 
 async function check(tag) {
-	return !!customElements?.get?.(tag);
+  return !!customElements?.get?.(tag);
 }
 
 async function* render(tag, attrs, children) {
-	const instance = new CustomElementRender(tag);
+  const instance = new CustomElementRender(tag);
 
-	Object.entries(attrs).forEach(([k,v]) => {
-		instance.setAttribute(k, v);
-	});
+  Object.entries(attrs).forEach(([k, v]) => {
+    instance.setAttribute(k, v);
+  });
 
-	if(children) {
+  if (children) {
     const nodes = new DOMParser().parseFromString(children, 'text/html').childNodes;
     instance.element.append(...nodes);
   }
 
-	instance?.connectedCallback?.();
+  instance?.connectedCallback?.();
 
-	yield `<${tag}`;
-	yield* instance.renderAttributes();
-	yield `>`;
-	const shadowContents = instance.renderShadow();
-	if (shadowContents !== undefined) {
-		yield '<template shadowroot="open">';
-		yield* shadowContents;
-		yield '</template>';
-	}
-	yield* instance.renderLight();
-	yield `</${tag}>`;
+  yield `<${tag}`;
+  yield* instance.renderAttributes();
+  yield `>`;
+  const shadowContents = instance.renderShadow();
+  if (shadowContents !== undefined) {
+    yield '<template shadowroot="open">';
+    yield* shadowContents;
+    yield '</template>';
+  }
+  yield* instance.renderLight();
+  yield `</${tag}>`;
 }
 
 async function renderToStaticMarkup(tag, attrs, children) {
 
-	let html = '';
-	for await (let chunk of render(tag, attrs, children)) {
-		html += chunk;
-	}
+  let html = '';
+  for await (let chunk of render(tag, attrs, children)) {
+    html += chunk;
+  }
 
-	return { html };
+  return { html };
 }
 
 export default { check, renderToStaticMarkup };
